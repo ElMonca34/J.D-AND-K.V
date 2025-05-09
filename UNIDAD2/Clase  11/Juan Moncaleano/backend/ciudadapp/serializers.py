70fac95b8ccbc8dd0fdc1e4c99e8b987b0ac3558
KarenVelasquez01 <karen.viviana.velasquez@correounivalle.edu.co>
2025-05-09 17:59:32 -0500

    Delete UNIDAD2/Clase  11/Juan Moncaleano directory

--- UNIDAD2/Clase  11/Juan Moncaleano/backend/ciudadapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import  ciudad
-
-class ciudadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ciudad
-        fields = '__all__'
--- UNIDAD2/Clase  11/Juan Moncaleano/backend/ciudadapp/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import ciudad
-from .serializers import ciudadSerializer
-
-class ciudadViewSet(viewsets.ModelViewSet):
-    queryset = ciudad.objects.all()
-    serializer_class = ciudadSerializer
