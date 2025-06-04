c78c29fbe2e980bf2cf8c6a4b238143b3d6be6e6
ElMonca34 <moncaleano.juan@correounivalle.edu.co>
2025-06-03 20:27:47 -0500

    Delete UNIDAD2/Proyecto final directory

--- UNIDAD2/Proyecto final/backend/autor/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets 
-from .models import Autor
-from .serializers import AutorSerializer
-
-class AutorViewSet(viewsets.ModelViewSet):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializer
